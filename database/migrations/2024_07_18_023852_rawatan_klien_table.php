78255a13b4ad2061bdaadbc6f9303139915a48fd
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2024-07-18 12:30:59 +0800

    update db 3

---
+++ database/migrations/2024_07_18_023852_rawatan_klien_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rawatan_klien', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('klien_id');
+            $table->string('tkh_perintah');
+            $table->date('tkh_mula_pengawasan');
+            $table->date('tkh_tamat_pengawasan');
+            $table->string('seksyen');
+            $table->string('puspen');
+            $table->string('pejabat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rawatan_klien');
+    }
+};
