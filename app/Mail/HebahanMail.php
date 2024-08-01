878f5db266ac06220458396445f7d6252192cab3
nfarahz <norfarahziba@graduate.utm.my>
2024-08-01 12:53:04 +0800

    push

--- app/Mail/HebahanMail.php
+++ app/Mail/HebahanMail.php
@@ -15,10 +15,22 @@ class HebahanMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $id;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
     public function build()
     {
         return $this->view('pengurusan_program.hebahan.emel')
-            ->attach(public_path('qr_codes/qrcode.png'), [
+            ->attach(public_path('qr_codes/qr_pengesahan_'.$this->id.'.png'), [
                 'as' => 'logo.png',
                 'mime' => 'image/png',
             ]);
