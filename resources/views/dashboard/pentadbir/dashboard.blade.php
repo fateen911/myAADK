a1192995a844deea2646600efe85dbdc4844335b
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2024-09-17 22:59:52 +0800

    Update dashboard.blade.php

--- resources/views/dashboard/pentadbir/dashboard.blade.php
+++ resources/views/dashboard/pentadbir/dashboard.blade.php
@@ -135,7 +135,7 @@
                                     <!--begin::Stats-->
                                     <div class="m-0">
                                         <a href={{ route('senarai-pengguna') }}>
-                                            <span id="klienCount" class="text-white fw-bolder d-block fs-4x lh-1 ls-n1 mb-1">{{$klien}}</span>
+                                            <span id="klienCount" class="text-white fw-bolder d-block fs-4x lh-1 ls-n1 mb-1">{{$jumlah1}}</span>
                                             <span class="text-white fw-bold fs-7">Klik untuk Lihat</span>
                                         </a>
                                     </div>
