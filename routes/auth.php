d03d1b45383c84dd6c7bd7a2d59cf7342bd9951e
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-09-18 23:32:31 +0800

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -42,6 +42,10 @@ Route::middleware('guest')->group(function () {
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
                 ->name('password.reset');
+
+    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');            
 });
 
 Route::middleware('auth')->group(function () {
@@ -59,9 +63,9 @@ Route::middleware('auth')->group(function () {
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
+    // Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    //             ->middleware(['signed', 'throttle:6,1'])
+    //             ->name('verification.verify');
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
                 ->middleware('throttle:6,1')
