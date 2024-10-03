1b1c9d8c2441c4bf2ecb2adbda4c223ff1c261f2
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2024-10-03 16:08:50 +0800

    Update view.blade.php

--- resources/views/profil_klien/klien/view.blade.php
+++ resources/views/profil_klien/klien/view.blade.php
@@ -8,6 +8,15 @@
 
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/datetime/1.5.1/js/dataTables.dateTime.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 
     <style>
@@ -20,12 +29,12 @@
         }
 
         /* Flexbox settings for the tab content container */
-        .tab-pane {
+        /* .tab-pane {
             display: flex;
             justify-content: center;
             align-items: start; 
             min-height: 100vh; 
-        }
+        } */
 
         /* Centered form settings */
         .centered-form {
