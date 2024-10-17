19bbbec7d612af5926969720d83a16a44acae6f5
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2024-10-17 16:59:10 +0800

    Update emel_daftar_pegawai_lulus.blade.php

--- resources/views/pendaftaran/emel_daftar_pegawai_lulus.blade.php
+++ resources/views/pendaftaran/emel_daftar_pegawai_lulus.blade.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
 <html>
-<body>
-    <h3>PENDAFTARAN PEGAWAI SEBAGAI PENGGUNA SISTEM MySupport AADK DILULUSKAN</h3>
-    <p>Assalamualaikum WBT & Salam Sejahtera,</p>
-    <p>Tuan/Puan,</p>
-    <p>Dimaklumkan bahawa pendaftaran akaun tuan/puan telah <b>diluluskan</b>. Sila gunakan maklumat yang disertakan di bawah untuk mendaftar masuk ke dalam sistem tersebut:</p>
-    <p style="padding-top: 10px"><b>Nama Pegawai :</b> {{strtoupper($nama)}}</p>
-    <p><b>No. Kad Pengenalan :</b> {{$no_kp}}
-    <p style="padding-bottom: 10px;"><b>Kata Laluan :</b> {{$password}}
-    <p>Sila klik pada pautan yang diberikan untuk mengesahkan emel anda dan log masuk ke dalam sistem.
-        @component('mail::button', ['url' => $verificationUrl])
-        @endcomponent
-    <br>
-    <p>Jika mempunyai sebarang pertanyaan, sila hubungi <i>hotline</i> di talian 1-800-22-2235 / 019 – 626 2233 atau e-mel ke webmaster@adk.gov.my </p>
-    <p>Untuk maklumat lanjut, sila layari https://www.adk.gov.my </p>
-    <br>
-    <p>Sekian, terima kasih.</p>
-    <br>
-    
-    <p><b>Agensi Antidadah Kebangsaan</b></p>
-</body>
+    <body>
+        <h3>PENDAFTARAN PEGAWAI SEBAGAI PENGGUNA SISTEM MySupport AADK DILULUSKAN</h3>
+        <p>Assalamualaikum WBT & Salam Sejahtera,</p>
+        <p>Tuan/Puan,</p>
+        <p>Dimaklumkan bahawa pendaftaran akaun tuan/puan telah <b>diluluskan</b>. Sila gunakan maklumat yang disertakan di bawah untuk mendaftar masuk ke dalam sistem tersebut:</p>
+        <p style="padding-top: 10px"><b>Nama Pegawai :</b> {{strtoupper($nama)}}</p>
+        <p><b>No. Kad Pengenalan :</b> {{$no_kp}}
+        <p style="padding-bottom: 10px;"><b>Kata Laluan :</b> {{$password}}
+        <p>Sila <a href="{{ $verificationUrl }}">klik di sini </a>untuk mengesahkan emel anda dan log masuk ke dalam sistem.</p>            
+        <br>
+        <p>Jika mempunyai sebarang pertanyaan, sila hubungi <i>hotline</i> di talian 1-800-22-2235 / 019 – 626 2233 atau e-mel ke webmaster@adk.gov.my </p>
+        <p>Untuk maklumat lanjut, sila layari https://www.adk.gov.my </p>
+        <br>
+        <p>Sekian, terima kasih.</p>
+        <br>
+        
+        <p><b>Agensi Antidadah Kebangsaan</b></p>
+    </body>
 </html>
