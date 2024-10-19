6ff45be75de2502a95fa12e716ab926404365533
nfarahz <norfarahziba@graduate.utm.my>
2024-10-19 12:58:33 +0800

    Update emel.blade.php

--- resources/views/pengurusan_program/hebahan/emel.blade.php
+++ resources/views/pengurusan_program/hebahan/emel.blade.php
@@ -5,13 +5,13 @@
 </head>
 <body>
 <p>Assalamualaikum dan Salam Sejahtera,<br>
-<p>Dengan segala hormatnya, kami ingin menjemput pihak tuan/puan untuk menghadiri program berikut:</p> <br>
+<p>Dengan segala hormatnya, kami ingin menjemput pihak tuan/puan untuk menghadiri aktiviti berikut:</p> <br>
 <p><b>NAMA AKTIVITI: {{strtoupper($program->nama)}}</b></p>
 <p><b>TARIKH MULA: {{date('d/m/Y, h:iA', strtotime($program->tarikh_mula))}}</b></p>
 <p><b>TARIKH TAMAT: {{date('d/m/Y, h:iA', strtotime($program->tarikh_tamat))}}</b></p>
 <p><b>TEMPAT: {{strtoupper($program->tempat)}}</b></p>
 <br>
-<p>Sila imbas kod QR atau klik pautan berikut untuk pengesahan kehadiran anda:</p>
+<p>Sila imbas kod QR atau klik pautan berikut untuk maklum balas kehadiran anda:</p>
 <img src="{{ $message->embed(public_path('qr_codes/qr_pengesahan_'.$program->id.'.png')) }}" alt="Logo">
 <br>
 <p>Pautan: <a href="{{$program->pautan_pengesahan}}">sila klik sini</a></p>
