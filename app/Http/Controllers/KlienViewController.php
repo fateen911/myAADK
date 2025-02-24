c313d68e5f0b915cc12dca7322ce056882822827
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-02-24 16:10:32 +0800

    Update KlienViewController.php

--- app/Http/Controllers/KlienViewController.php
+++ app/Http/Controllers/KlienViewController.php
@@ -11,18 +11,22 @@ class KlienViewController extends Controller
     public function viewKlien()
     {
         // $data = KlienView::limit(20)->get()->toArray(); // Retrieve limited data for testing
-        $data = KlienView::whereIn('id_fasiliti', ['16', '31', '45', '57', '69', '80', '90', '99', '106'])
-                ->where('tkh_tamatPengawasan', '<=', '2025-04-01')
-                ->whereIn(DB::raw('(id_pk, tkh_tamatPengawasan)'), function ($query) {
-                    $query->selectRaw('id_pk, MAX(tkh_tamatPengawasan)')
-                        ->from('view_pccp_klien')
-                        ->where('tkh_tamatPengawasan', '<=', '2025-04-01')
-                        ->whereIn('id_fasiliti', ['16', '31', '45', '57', '69', '80', '90', '99', '106'])
-                        ->groupBy('id_pk');
-                })
-                ->get()
-                ->limit(15000)
-                ->toArray();
+        $subQuery = DB::table('view_pccp_klien')
+        ->select('id_pk', DB::raw('MAX(tkh_tamatPengawasan) as latest_tkh_tamatPengawasan'))
+        ->where('tkh_tamatPengawasan', '<=', '2025-04-01')
+        ->whereIn('id_fasiliti', ['16', '31', '45', '57', '69', '80', '90', '99', '106'])
+        ->groupBy('id_pk');
+    
+        $data = KlienView::joinSub($subQuery, 'latest', function ($join) {
+                $join->on('klien_view.id_pk', '=', 'latest.id_pk')
+                    ->on('klien_view.tkh_tamatPengawasan', '=', 'latest.latest_tkh_tamatPengawasan');
+            })
+            ->whereIn('klien_view.id_fasiliti', ['16', '31', '45', '57', '69', '80', '90', '99', '106'])
+            ->limit(1000) 
+            ->get()
+            ->toArray();
+    
+
 
         // Insert the retrieved data into the viewKlien table
         if (!empty($data)) {
