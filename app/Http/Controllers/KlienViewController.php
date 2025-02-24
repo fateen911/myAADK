43a01c75c68d785de9a32fe84c5644d29439116c
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-02-24 16:48:00 +0800

    Update KlienViewController.php

--- app/Http/Controllers/KlienViewController.php
+++ app/Http/Controllers/KlienViewController.php
@@ -10,15 +10,19 @@ class KlienViewController extends Controller
 {
     public function viewKlien()
     {
-        $data = KlienView::whereIn('id_fasiliti', ['31'])
+        $data = KlienView::where('id_fasiliti', '31')
             ->where('tkh_tamatPengawasan', '<=', '2025-04-01')
-            ->limit(5000)        
+            ->limit(5000)
             ->get()
             ->toArray();
 
         if (!empty($data)) {
-            DB::table('viewKlien')->insert($data);
+            // Insert data in chunks of 500 to avoid database limits
+            collect($data)->chunk(500)->each(function ($chunk) {
+                DB::table('viewKlien')->insert($chunk->toArray());
+            });
         }
+
 
         // Pass the data to the view
         return view('secondDB.view_klien', compact('data'));
