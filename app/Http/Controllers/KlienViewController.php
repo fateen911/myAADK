548ad646ebcb78cde3a0de5bdfc5231fec53c662
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-02-24 15:28:40 +0800

    Update KlienViewController.php

--- app/Http/Controllers/KlienViewController.php
+++ app/Http/Controllers/KlienViewController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\KlienView;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class KlienViewController extends Controller
 {
     public function viewKlien()
     {
-        $data = KlienView::limit(20)->get()->toArray(); // Retrieve limited data for testing
+        // $data = KlienView::limit(20)->get()->toArray(); // Retrieve limited data for testing
+        $data = KlienView::whereIn('id_fasiliti', ['16', '31', '45', '57', '69', '80', '90', '99', '106'])
+                ->get()
+                ->toArray();
+
+        // Insert the retrieved data into the viewKlien table
+        if (!empty($data)) {
+            DB::table('viewKlien')->insert($data);
+        }
         return view('secondDB.view_klien', compact('data'));
     }
 }
