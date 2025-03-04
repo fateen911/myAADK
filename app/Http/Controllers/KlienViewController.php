e720c9f2d649165c593cbc32fe2b843e7f4986f6
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-03-05 00:31:52 +0800

    Update KlienViewController.php

--- app/Http/Controllers/KlienViewController.php
+++ app/Http/Controllers/KlienViewController.php
@@ -10,7 +10,7 @@ class KlienViewController extends Controller
 {
     public function viewKlien()
     {
-        $data = KlienView::where('AADK_Negeri', 'Negeri Sembilan')
+        $data = KlienView::where('AADK_Negeri', 'Kelantan')
             ->where('tkh_tamatPengawasan', '<=', '2025-04-01')
             ->limit(50000)
             ->get()
