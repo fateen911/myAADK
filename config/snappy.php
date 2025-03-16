58daadc6704f0ff2f4f8e5b62f4f2a0a39432d37
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2025-03-17 01:42:38 +0800

    Update snappy.php

--- config/snappy.php
+++ config/snappy.php
@@ -35,12 +35,12 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'binary'  => env('WKHTML_PDF_BINARY', '/usr/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
     ],
-
+    
     'image' => [
         'enabled' => true,
         'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
