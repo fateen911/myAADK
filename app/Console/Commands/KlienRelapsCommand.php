b57679cbc5df6717be1fef5668f45c99d10af455
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-06-11 23:37:51 +0800

    KlienRelapsCommand

---
+++ app/Console/Commands/KlienRelapsCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Models\viewklienlocal;
+use App\Models\KlienView;
+use Illuminate\Support\Facades\App;
+
+class KlienRelapsCommand extends Command
+{
+    protected $signature = 'klien:relaps';
+    protected $description = 'Check and freeze user accounts if not found in KlienView';
+
+    public function handle()
+    {
+        $users = User::where('tahap_pengguna', 2)->get();
+
+        foreach ($users as $user) {
+            $no_kp = $user->no_kp;
+
+            if (App::environment('local')) {
+                //LOCAL
+                $klienData = viewklienlocal::where('mykad', $no_kp)->first();
+            } else {
+                //SERVER
+                $klienData = KlienView::where('mykad', $no_kp)->first();
+            }
+
+            if (!$klienData) {
+                $user->update([
+                    'acc_status' => 'DIBEKUKAN',
+                    'updated_at' => now(),
+                ]);
+
+                $this->info("User {$user->id} dibekukan.");
+            }
+        }
+
+        $this->info('KlienRelapsCommand selesai.');
+    }
+}
+
